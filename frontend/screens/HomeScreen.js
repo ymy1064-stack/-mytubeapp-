798a67c92d4c916c8d81e8ddc66a23aa671dcbbd
Lamyahedlatif <ymy1064@gmail.com>
2025-08-28 23:10:35 +0530

    Update HomeScreen.js

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
 
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 18 }}>Welcome to MyYouTubeApp 🚀</Text>
+      <Text style={{ fontSize: 14, marginTop: 10 }}>Choose a tab below to get started</Text>
+    </View>
+  );
+  }
