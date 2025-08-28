bf8486e286ba6728b15e50f18dec0fc595205498
Lamyahedlatif <ymy1064@gmail.com>
2025-08-28 23:15:27 +0530

    Create Footer.js

---
+++ frontend/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Footer() {
+  return (
+    <View style={{ padding: 10, alignItems: "center" }}>
+      <Text style={{ fontSize: 10, color: "gray" }}>© 2025 MyYouTubeApp</Text>
+    </View>
+  );
+                   }
